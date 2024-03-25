4bea076f8fcbdf60c34537c23f7698a2ebe89d11
Tayor99 <adetayo@flanceapp.com>
2024-03-25 23:57:52 +0100

    initail setup

---
+++ components/Hello.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export default function Hello() {
+  return (
+    <View>
+      <Text className="text-red-400">Hello</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});/
---
+++ screens/HomeScreens.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreens() {
+  return (
+    <View>
+      <Text className="text-red-400">HomeScreens</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});
